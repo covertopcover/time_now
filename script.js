e3c26ee7b8410a18deb309cf672cfcfa4b5ead4b
covertopcover <tadas.pukas@gmail.com>
2024-12-08 12:57:41 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateTime() {
+    const clockElement = document.getElementById('clock');
+    const currentTime = new Date();
+    clockElement.textContent = currentTime.toLocaleTimeString();
+}
+
+// Update time every second
+setInterval(updateTime, 1000);
+
+// Initial call to display time immediately
+updateTime();
