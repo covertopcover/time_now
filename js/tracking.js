975a863b057a221fa21f13686598baffa0886b0b
covertopcover <tadas.pukas@gmail.com>
2025-01-01 20:04:42 +0200

    Google Analytics

--- js/tracking.js
+++
@@ -1,6 +0,0 @@
-// Initialize Mixpanel with your project token
-mixpanel.init("5f6a9500d7a08460964b66e27566ca2b", {
-    debug: false,
-    track_pageview: true,
-    persistence: "localStorage"
-});
